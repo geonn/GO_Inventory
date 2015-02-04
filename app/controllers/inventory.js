6464c8a95b6ce95f89b14d4823a58280b5f13c76
lauyikonn <lau.yikonn@gmail.com>
2015-02-05 01:25:39 +0800

    inventory

--- app/controllers/inventory.js
+++ app/controllers/inventory.js
@@ -37,77 +37,122 @@ function displayProduct(products){
 	   			 
 	   			var row = Titanium.UI.createTableViewRow({
 			    touchEnabled: true,
-			    height: 70,
 			    source: entry.code,
 			    selectedBackgroundColor: "#ECFFF9",
 				backgroundGradient: {
-			      type: 'linear',
-			      colors: ['#FEFEFB','#F7F7F6'],
-			      startPoint: {x:0,y:0},
-			      endPoint:{x:0,y:70},
-			      backFillStart:false},
+				      type: 'linear',
+				      colors: ['#fff','#F7F7F6'],
+				      startPoint: {x:0,y:0},
+				      endPoint:{x:"100%",y:0},
+				      backFillStart:false
+				    },
 			    });
+				
+				var row_view = Ti.UI.createView({
+					left: 5,
+			    	top: 5, 
+			    	right: 5,
+			    	bottom: 5,
+			    	height: 80,
+			    	width: Ti.UI.FILL,
+			    	layout: "horizontal",
+				});
 				
 				var tblView = Ti.UI.createView({
 					layout: "vertical",
 					height:"80",
-					width:"auto" 
+					width:"auto",
 				}); 
 				
 				var leftImage =  Titanium.UI.createImageView({
 					image:entry.image,
 					source: entry.id,
-					width:"20%",
-					height:50,
-					left:10,
-					top:10
+					width: 112,
+					right: 10,
+					height: 80,
 				});	
 		 
 				var popUpTitle = Titanium.UI.createLabel({
 					text:entry.name,
-					font:{fontSize:16},
+					font:{fontSize:14, fontWeight:'bold'},
 					source: entry.id,
 					color: "#848484",
-					width:'50%',
-					textAlign:'left',
-					top:15,
-					left:80,
-					height:Ti.UI.SIZE
+					width: Ti.UI.FILL,
+					height: Ti.UI.SIZE,
+					textAlign:Titanium.UI.TEXT_ALIGNMENT_LEFT,
+					
 				});
 				
 				var category =  Titanium.UI.createLabel({
 					text:entry.category,
 					source: entry.id,
-					font:{fontSize:12,fontWeight:'bold'},
-					width:'auto',
+					font:{fontSize:11},
 					color: "#848484",
-					textAlign:'left',
-					width:'50%', 
-					left:80,
-					height:Ti.UI.SIZE
+					textAlign: Titanium.UI.TEXT_ALIGNMENT_LEFT,
+					width: Ti.UI.FILL,
+					height: Ti.UI.SIZE,
 				});
 				
 				var distance =  Titanium.UI.createLabel({
 					text:entry.code,
 					source: entry.id,
-					font:{fontSize:12,fontWeight:'bold'},
-					width:'50%',
+					font:{fontSize:11},
 					color: "#848484",
-					textAlign:'left',  
-					left:80,
-					height:Ti.UI.SIZE
+					textAlign: Titanium.UI.TEXT_ALIGNMENT_LEFT,
+					width: Ti.UI.FILL,
+					height: Ti.UI.SIZE,
 				}); 
 				
+				var quantity_view = Ti.UI.createView({
+					layout: "horizontal",
+					width: Ti.UI.FILL,
+					height: Ti.UI.SIZE,
+					top: 5,
+				});
+				
+				var quantity_text_view = Ti.UI.createView({
+					width: Ti.UI.SIZE,
+					height: Ti.UI.SIZE,
+					backgroundColor: "red",
+				});
 				 
+				var quantity_text =  Titanium.UI.createLabel({
+					text: "QUANTITY",
+					source: entry.id,
+					font:{fontSize:14},
+					left: 5,
+					right: 5,
+					color: "#ffffff",
+					textAlign: Titanium.UI.TEXT_ALIGNMENT_LEFT,
+					width: Ti.UI.SIZE,
+					height: 20,
+				});
+				
+				var quantity_text_input = Titanium.UI.createTextField({
+					editable: false,
+					borderColor: "red",
+					height: 20,
+					width: Ti.UI.FILL,
+					value: 1000,
+					textAlign: Titanium.UI.TEXT_ALIGNMENT_CENTER,
+				});
+				
+				quantity_text_view.add(quantity_text);
+				quantity_view.add(quantity_text_view);
+				quantity_view.add(quantity_text_input);
+				
 				row.addEventListener('touchend', function(e) {
 				 	viewDetails(e);
 				});
-			 
-				row.add(leftImage);
+			 	
 				tblView.add(popUpTitle);
 				tblView.add(category);
 			 	tblView.add(distance);  
-			 	row.add(tblView);
+			 	tblView.add(quantity_view);
+			 	
+			 	row_view.add(leftImage);
+			 	row_view.add(tblView);
+			 	row.add(row_view);
 			 	data.push(row);
 	   		});
 	   		
